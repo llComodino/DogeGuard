ce8afd792065306960b02ec2b94f9b87fd9dd9c9
llComodino <comodinocode@proton.me>
2023-11-12 18:23:07 +0100

    Switching to Rocket.rs

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,53 @@
-// Multithreaded TCP server implementing:
+// Multithreaded rocket HTTP server implementing:
 // - Client authentication
 // - Client registration
 // - Client login
 // - Client logout
 // - Client message sending
 // - Client message receiving
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    
-    loop {
-        
-        let (mut socket, _) = listener.accept().await?;
-        
-        tokio::spawn(async move {
 
-            let mut buf = [0; 1024];
+// Rocket imports
+// Macros
+#[macro_use] extern crate rocket;
+use rocket::State;
+use rocket::response::content;
+/*
+use rocket::http::RawStr;
+use rocket::http::hyper::StatusCode::InternalServerError;
+use rocket::response::NamedFile;
+use rocket::response::Redirect;
+use rocket::response::status;
+use rocket::response::status::NotFound;
+use rocket::response::status::Unauthorized;
+use rocket::response::status::BadRequest;
+use rocket::response::status::Created;
+use rocket::response::status::Accepted;
+use rocket::response::status::Forbidden;
 
-            // In a loop, read data from the socket and write the data back.
-            loop {
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::oneshot;
+use tokio::sync::oneshot::Sender as OneshotSender;
+use tokio::sync::oneshot::Receiver as OneshotReceiver;
+*/
+#![feature(decl_macro)]
+#[get("/")]
+fn index() -> content::Html<&'static str> {
+    content::Html(include_str!("./www/index.html"))
+}
 
-                // Ask for client authentication
-                if let Err(e) = socket.write_all(b"Please authenticate yourself\n").await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+#[launch]
+fn rocket() -> _ {
+    rocket::build().manage(State::new())
+}
 
-                // Read the data
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                println!("received {} bytes from socket", n);
-                println!("data: {:?}", &buf[0..n]);
-                let content = String::from_utf8_lossy(&buf[0..n]);
-                println!("content: {:?}", content);
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
+#[tokio::main]
+async fn main() {
+    // Rocket server configuration
+    rocket::ignite()
+        .mount("/", routes![index])
+        .launch()
+        .await
+        .expect("Failed to launch Rocket server");
 }
