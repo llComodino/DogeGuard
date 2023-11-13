75f226a4cf86dd78b8de81e871e83b6a3835a656
llComodino <comodinocode@proton.me>
2023-11-13 22:59:49 +0100

    Nuked the old project

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,88 @@
-// Multithreaded rocket HTTP server implementing:
-// - Client authentication
-// - Client registration
-// - Client login
-// - Client logout
-// - Client message sending
-// - Client message receiving
-
-// Rocket imports
-// Macros
-#[macro_use] extern crate rocket;
-use rocket::State;
-use rocket::response::content;
 /*
-use rocket::http::RawStr;
-use rocket::http::hyper::StatusCode::InternalServerError;
-use rocket::response::NamedFile;
-use rocket::response::Redirect;
-use rocket::response::status;
-use rocket::response::status::NotFound;
-use rocket::response::status::Unauthorized;
-use rocket::response::status::BadRequest;
-use rocket::response::status::Created;
-use rocket::response::status::Accepted;
-use rocket::response::status::Forbidden;
+ * This server waits for a connection on port 8080
+ * then spawns a thread to handle the connection
+ *
+ * The thread is a client handler that reads the request
+ * The client can:
+ * 1. Search for a value in the database
+ * 2. Add a value to the database
+ * 3. Delete a value from the database
+ * 4. List all values in the database
+ * 5. Exit
+ *
+ * The database stores the following:
+ * 1. Website name
+ * 2. username
+ * 3. password
+ * 4. url
+ *
+ * The database is hosted on a remote server
+ * The client can connect to the server and perform the above actions
+ * The server will return the result of the action 
+ * The client will display the result to the user 
+ * The client will then prompt the user for another action 
+ * The client will continue to prompt the user until the user exits
+ * The client will then close the connection to the server
+ * The client will then exit 
+ * 
+ */
 
-use tokio::sync::mpsc;
-use tokio::sync::mpsc::Sender;
-use tokio::sync::mpsc::Receiver;
-use tokio::sync::oneshot;
-use tokio::sync::oneshot::Sender as OneshotSender;
-use tokio::sync::oneshot::Receiver as OneshotReceiver;
-*/
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+use std::str;
+use std::process::Command;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::ErrorKind;
+use std::io::Error;
+use std::path::Path;
+use std::fs::OpenOptions;
 
-#[get("/")]
-fn index() -> content::Html<&'static str> {
-    content::Html(include_str!("./www/index.html"))
+// This function handles the client 
+fn handle_client(mut stream: TcpStream) {
+    // Create a buffer to store the data from the client
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+
+    // Read the data from the client
+    match stream.read(&mut data) {
+        Ok(size) => {
+            // Convert the data to a string
+            let s = match str::from_utf8(&data[0..size]) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+
+            // Print the data to the console
+            println!("{}", s);
+
+            // Parse the data
+            let mut split = s.split_whitespace();
+            let command = split.next().unwrap();
+            let arg = split.next().unwrap_or("");
+        }
+    }
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().manage(State::new())
-}
+fn main() {
+    // Create a listener on port 8080
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-#[tokio::main]
-async fn main() {
-    // Rocket server configuration
-    rocket::ignite()
-        .mount("/", routes![index])
-        .launch()
-        .await
-        .expect("Failed to launch Rocket server");
+    // Wait for connections
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Spawn a thread to handle the connection
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => { /* connection failed */ }
+        }
+    }
+
+    // close the socket server
+    drop(listener);
 }
